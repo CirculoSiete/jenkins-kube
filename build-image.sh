b459d0cdc7a11e48db63590c9abb18593d89bcdc
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-08-19 12:51:10 -0500

    upgraded to Jenkins 2.190-jdk11

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-JENKINS_VERSION=2.189-jdk11
+JENKINS_VERSION=2.190-jdk11
 
 docker build --build-arg JENKINS_VERSION=${JENKINS_VERSION} -t circulo7/jenkins_kube:${JENKINS_VERSION} . && docker push circulo7/jenkins_kube:${JENKINS_VERSION} && git release $JENKINS_VERSION

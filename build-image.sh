750599270ba96861f9ea5aa3d92aa00428d53c59
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-08-28 18:41:48 -0500

    Added docker-compose

--- build-image.sh
+++ build-image.sh
@@ -2,4 +2,9 @@
 
 JENKINS_VERSION=2.192-jdk11
 
-docker build --build-arg JENKINS_VERSION=${JENKINS_VERSION} -t circulo7/jenkins_kube:${JENKINS_VERSION} -t circulo7/jenkins_kube:latest . && docker push circulo7/jenkins_kube:${JENKINS_VERSION} && docker push circulo7/jenkins_kube:latest && git release $JENKINS_VERSION
+docker build --build-arg JENKINS_VERSION=${JENKINS_VERSION} \
+  -t circulo7/jenkins_kube:${JENKINS_VERSION} \
+  -t circulo7/jenkins_kube:latest . && \
+  docker push circulo7/jenkins_kube:${JENKINS_VERSION} && \
+  docker push circulo7/jenkins_kube:latest && \
+  git release $JENKINS_VERSION

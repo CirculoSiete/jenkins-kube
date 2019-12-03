0cab721bd3ac011fb56ce2e7d5bfb3ee8007aa74
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-12-03 11:12:04 -0600

    upgraded to Jenkins 2.207-jdk11

--- build-image.sh
+++ build-image.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-JENKINS_VERSION=2.206-jdk11
+JENKINS_VERSION=2.207-jdk11
+
+LOCAL_TAG_VERSION="${TAG_VERSION:-$JENKINS_VERSION}"
+
+echo "Jenkins version: $JENKINS_VERSION"
+echo "LOCAL_TAG_VERSION: $LOCAL_TAG_VERSION"
+echo $TAG_VERSION
 
 docker build --build-arg JENKINS_VERSION=${JENKINS_VERSION} \
-  -t circulo7/jenkins_kube:${JENKINS_VERSION} \
-  -t circulo7/jenkins_kube:latest . && \
-  git release $JENKINS_VERSION && \
-  docker push circulo7/jenkins_kube:${JENKINS_VERSION} && \
-  docker push circulo7/jenkins_kube:latest
+   -t circulo7/jenkins_kube:${LOCAL_TAG_VERSION} \
+   -t circulo7/jenkins_kube:latest . && \
+   git release $LOCAL_TAG_VERSION && \
+   docker push circulo7/jenkins_kube:${LOCAL_TAG_VERSION} && \
+   docker push circulo7/jenkins_kube:latest && \
+   echo "Done"
